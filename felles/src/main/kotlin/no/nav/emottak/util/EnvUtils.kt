ae887dfa9c7e6750de2e547b1f2862197ca95793
terjenilssen <terje.nilssen@nav.no>
2025-01-10 16:15:06 +0100

    Lagt inn exclude på "emottak-payload-xsd" fra felles-modulen i "smtp-transport", for at MailReader ikke skal få ClassNotFoundException på javax.activation.DataContentHandler. Har i tillegg aktivert ktlintCheck i felles-modulen.

--- felles/src/main/kotlin/no/nav/emottak/util/EnvUtils.kt
+++ felles/src/main/kotlin/no/nav/emottak/util/EnvUtils.kt
@@ -9,7 +9,7 @@ fun String.fromEnv(): String = getEnvVar(this)
 
 fun isProdEnv(): Boolean = getEnvVar("NAIS_CLUSTER_NAME", "local") == "prod-fss"
 
-fun getSecret(secretPath:String, localValue: String) = when (getEnvVar("NAIS_CLUSTER_NAME", "local")) {
+fun getSecret(secretPath: String, localValue: String) = when (getEnvVar("NAIS_CLUSTER_NAME", "local")) {
     "local" -> localValue
     else -> String(FileInputStream(secretPath).readAllBytes())
 }
