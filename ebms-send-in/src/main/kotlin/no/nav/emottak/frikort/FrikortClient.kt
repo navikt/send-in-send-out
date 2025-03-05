32b68908c25754087c8dd517f45e073540ed4983
Ivan Skodje <ivanskodje@protonmail.com>
2025-03-05 08:54:59 +0100

    refactor: changed non-prod info logs to debug logs

--- ebms-send-in/src/main/kotlin/no/nav/emottak/frikort/FrikortClient.kt
+++ ebms-send-in/src/main/kotlin/no/nav/emottak/frikort/FrikortClient.kt
@@ -1,17 +1,16 @@
 package no.nav.emottak.frikort
 
+import javax.xml.namespace.QName
 import no.nav.emottak.cxf.ServiceBuilder
 import no.nav.emottak.ebms.log
 import no.nav.emottak.fellesformat.FellesFormatXmlMarshaller
 import no.nav.emottak.utils.getEnvVar
 import no.nav.emottak.utils.getSecret
-import no.nav.emottak.utils.isProdEnv
 import no.nav.tjeneste.ekstern.frikort.v1.FrikortV1Port
 import no.nav.tjeneste.ekstern.frikort.v1.types.FrikortsporringMengdeResponse
 import no.nav.tjeneste.ekstern.frikort.v1.types.FrikortsporringResponse
 import no.nav.tjeneste.ekstern.frikort.v1.types.ObjectFactory
 import no.trygdeetaten.xml.eiff._1.EIFellesformat
-import javax.xml.namespace.QName
 
 val frikortClient = frikortEndpoint()
 private val frikortObjectFactory = ObjectFactory()
@@ -30,29 +29,21 @@ fun frikortEndpoint(): FrikortV1Port =
         .get()
 
 fun frikortsporring(fellesformat: EIFellesformat): FrikortsporringResponse {
-    if (!isProdEnv()) {
-        log.info("Sending in frikortsporring request with body: " + FellesFormatXmlMarshaller.marshal(fellesformat))
-    }
+    log.debug("Sending in frikortsporring request with body: " + FellesFormatXmlMarshaller.marshal(fellesformat))
 
     return frikortClient.frikortsporring(
         frikortObjectFactory.createFrikortsporringRequest().also { it.eiFellesformat = fellesformat }
     ).also {
-        if (!isProdEnv()) {
-            log.info("Send in Frikort response " + FellesFormatXmlMarshaller.marshal(it))
-        }
+        log.debug("Send in Frikort response " + FellesFormatXmlMarshaller.marshal(it))
     }
 }
 
 fun frikortsporringMengde(fellesformat: EIFellesformat): FrikortsporringMengdeResponse {
-    if (!isProdEnv()) {
-        log.info("Sending in frikortsporringMengde request with body: " + FellesFormatXmlMarshaller.marshal(fellesformat))
-    }
+    log.debug("Sending in frikortsporringMengde request with body: " + FellesFormatXmlMarshaller.marshal(fellesformat))
 
     return frikortClient.frikortsporringMengde(
         frikortObjectFactory.createFrikortsporringMengdeRequest().also { it.eiFellesformat = fellesformat }
     ).also {
-        if (!isProdEnv()) {
-            log.info("Send in FrikortMengde response " + FellesFormatXmlMarshaller.marshal(it))
-        }
+        log.debug("Send in FrikortMengde response " + FellesFormatXmlMarshaller.marshal(it))
     }
 }
