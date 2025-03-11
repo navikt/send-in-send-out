c851756a43ebb0eea23b5f62fcfe2684938796a4
Ivan Skodje <ivanskodje@protonmail.com>
2025-03-11 07:53:23 +0100

    fix: setting cpaID unless the service is PasientlisteForesporsel

--- ebms-send-in/src/main/kotlin/no/nav/emottak/fellesformat/FellesFormatWrapper.kt
+++ ebms-send-in/src/main/kotlin/no/nav/emottak/fellesformat/FellesFormatWrapper.kt
@@ -16,14 +16,20 @@ fun SendInRequest.asEIFellesFormat(): EIFellesformat =
         msgHead = unmarshal(this@asEIFellesFormat.payload.toString(Charsets.UTF_8), MsgHead::class.java)
     }
 
-private fun createFellesFormatMottakEnhetBlokk(sendInRequest: SendInRequest): EIFellesformat.MottakenhetBlokk =
-    fellesFormatFactory.createEIFellesformatMottakenhetBlokk().apply {
+private fun createFellesFormatMottakEnhetBlokk(sendInRequest: SendInRequest): EIFellesformat.MottakenhetBlokk {
+    val partnerReferanse = when (sendInRequest.addressing.service) {
+        "PasientlisteForesporsel" -> sendInRequest.partnerId?.toString() ?: ""
+        else -> sendInRequest.cpaId
+    }
+
+    return fellesFormatFactory.createEIFellesformatMottakenhetBlokk().apply {
         ebXMLSamtaleId = sendInRequest.conversationId
         ebAction = sendInRequest.addressing.action
         ebService = sendInRequest.addressing.service
         ebRole = sendInRequest.addressing.from.role
         avsender = "TODO1" // Hentes fra from. Usikker på hvilket felt siden det kan være flere.
-        avsenderRef = "TODO2" // Hentet fra cert: Eksempelverdi: "SERIALNUMBER=132547698, CN=Blå &amp; Bjørnebær AS, O=Blå &amp; Bjørnebær AS, C=NO"
+        avsenderRef =
+            "TODO2" // Hentet fra cert: Eksempelverdi: "SERIALNUMBER=132547698, CN=Blå &amp; Bjørnebær AS, O=Blå &amp; Bjørnebær AS, C=NO"
         mottaksId = sendInRequest.messageId
         mottattDatotid = Instant.now().toXMLGregorianCalendar()
         ediLoggId = sendInRequest.messageId
@@ -32,9 +38,9 @@ private fun createFellesFormatMottakEnhetBlokk(sendInRequest: SendInRequest): EI
         herIdentifikator = sendInRequest.addressing.from.partyId.getIdentifikatorByType("HER")
         orgNummer = sendInRequest.addressing.from.partyId.getIdentifikatorByType("orgnummer", "ENH")
         meldingsType = "xml"
-        partnerReferanse = sendInRequest.partnerId?.toString() ?: ""
+        this.partnerReferanse = partnerReferanse
     }
+}
 
-private fun List<PartyId>.getIdentifikatorByType(vararg types: String) = this.firstOrNull {
-    types.contains(it.type)
-}?.value ?: "Ukjent"
+private fun List<PartyId>.getIdentifikatorByType(vararg types: String) =
+    this.firstOrNull { types.contains(it.type) }?.value ?: "Ukjent"
