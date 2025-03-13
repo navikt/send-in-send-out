57cd20eb969271ad952dc6b5cee242f3df8f7802
Ivan Skodje <ivanskodje@protonmail.com>
2025-03-13 13:30:42 +0100

    refactor: added server config and coroutine friendly server setup

--- ebms-send-in/src/main/kotlin/no/nav/emottak/ebms/App.kt
+++ ebms-send-in/src/main/kotlin/no/nav/emottak/ebms/App.kt
@@ -1,15 +1,17 @@
 package no.nav.emottak.ebms
 
 import arrow.continuations.SuspendApp
+import arrow.continuations.ktor.server
 import arrow.core.raise.result
+import arrow.fx.coroutines.ResourceScope
 import arrow.fx.coroutines.resourceScope
 import io.ktor.server.application.Application
-import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 import io.ktor.utils.io.CancellationException
 import io.micrometer.prometheus.PrometheusConfig
 import io.micrometer.prometheus.PrometheusMeterRegistry
 import kotlinx.coroutines.awaitCancellation
+import no.nav.emottak.config.config
 import no.nav.emottak.ebms.plugin.configureAuthentication
 import no.nav.emottak.ebms.plugin.configureContentNegotiation
 import no.nav.emottak.ebms.plugin.configureCoroutineDebugger
@@ -22,14 +24,7 @@ internal val log = LoggerFactory.getLogger("no.nav.emottak.ebms.App")
 fun main() = SuspendApp {
     result {
         resourceScope {
-            val prometheusMeterRegistry = PrometheusMeterRegistry(PrometheusConfig.DEFAULT)
-
-            val server = embeddedServer(Netty, port = 8080) {
-                ebmsSendInModule(prometheusMeterRegistry)
-            }.also { it.engineConfig.maxChunkSize = 100000 }
-
-            server.start(wait = false)
-
+            setupServer()
             awaitCancellation()
         }
     }.onFailure { error ->
@@ -38,6 +33,20 @@ fun main() = SuspendApp {
             else -> logError(error)
         }
     }
+}
+
+suspend fun ResourceScope.setupServer() {
+    val server = config().server
+    System.setProperty("io.netty.maxChunkSize", server.maxChunkSize)
+
+    val prometheusMeterRegistry = PrometheusMeterRegistry(PrometheusConfig.DEFAULT)
+
+    server(
+        Netty,
+        port = server.port,
+        preWait = server.preWait,
+        module = { ebmsSendInModule(prometheusMeterRegistry) }
+    )
 }
 
 internal fun Application.ebmsSendInModule(meterRegistry: PrometheusMeterRegistry) {
