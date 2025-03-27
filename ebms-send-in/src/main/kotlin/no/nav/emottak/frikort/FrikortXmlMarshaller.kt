79126ede570c374af4a3eafcdabab3c8ce1eaf8a
Ivan Skodje <ivanskodje@protonmail.com>
2025-03-27 13:42:55 +0100

    chore: lint

--- ebms-send-in/src/main/kotlin/no/nav/emottak/frikort/FrikortXmlMarshaller.kt
+++ ebms-send-in/src/main/kotlin/no/nav/emottak/frikort/FrikortXmlMarshaller.kt
@@ -18,7 +18,7 @@ val FrikortXmlMarshaller = XmlMarshaller(
         no.kith.xmlstds.msghead._2006_05_24.ObjectFactory::class.java,
         org.w3._1999.xlink.ObjectFactory::class.java,
         org.w3._2009.xmldsig11_.ObjectFactory::class.java,
-        no.kith.xmlstds.nav.pasientliste._2010_02_01.PasientlisteForesporsel::class.java,
+        no.kith.xmlstds.nav.pasientliste._2010_02_01.PasientlisteForesporsel::class.java
     )
 )
 
