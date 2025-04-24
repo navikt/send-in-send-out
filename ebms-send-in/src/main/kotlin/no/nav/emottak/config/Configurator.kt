1851d93baa6e5a9fbf777d59f8e553f942a110df
Oleksandr Chmyr <oleksandr.chmyr@nav.no>
2025-04-24 10:43:02 +0200

    Tatt i bruk kafka_common.conf konfigurasjonsfilen

--- ebms-send-in/src/main/kotlin/no/nav/emottak/config/Configurator.kt
+++ ebms-send-in/src/main/kotlin/no/nav/emottak/config/Configurator.kt
@@ -9,6 +9,7 @@ import com.sksamuel.hoplite.addResourceSource
 val config: () -> Config = {
     ConfigLoader.builder()
         .addResourceSource("/application-personal.conf", optional = true)
+        .addResourceSource("/kafka_common.conf")
         .addResourceSource("/application.conf")
         .withExplicitSealedTypes()
         .build()
