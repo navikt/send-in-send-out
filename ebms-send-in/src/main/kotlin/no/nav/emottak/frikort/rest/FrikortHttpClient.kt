868da18de1ac7c694115eaefa09b4061cdd341c5
Thomas Burnett <thburnett@users.noreply.github.com>
2025-09-17 15:19:54 +0200

    Json debug logging

--- ebms-send-in/src/main/kotlin/no/nav/emottak/frikort/rest/FrikortHttpClient.kt
+++ ebms-send-in/src/main/kotlin/no/nav/emottak/frikort/rest/FrikortHttpClient.kt
@@ -21,14 +21,21 @@ import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
 import no.nav.emottak.melding.model.FrikortsporringRequest
 import no.nav.emottak.melding.model.FrikortsporringResponse
+import no.nav.emottak.util.LogLevel
+import no.nav.emottak.util.asJson
 import no.nav.emottak.utils.environment.getEnvVar
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import java.net.InetSocketAddress
 import java.net.Proxy
 import java.net.URI
+import kotlin.also
 
-val URL_FRIKORT_BASE = getEnvVar("URL_FRIKORT_REPO", "http://frikorttjenester.teamfrikort/api/ekstern/frikortsporringer")
-val URL_FRIKORT_HAR_BORGER_FRIKORT = getEnvVar("URL_FRIKORT_HARBORGER", "$URL_FRIKORT_BASE/harborgerfrikort")
-val URL_FRIKORT_HAR_BORGER_EGENANDELFRITAK = getEnvVar("URL_FRIKORT_HARBORGER", "$URL_FRIKORT_BASE/harborgeregenandelfritak")
+val URL_FRIKORT_BASE = getEnvVar("FRIKORT_URL_REST", "http://frikorttjenester.teamfrikort/api/ekstern/frikortsporringer")
+val URL_FRIKORT_HAR_BORGER_FRIKORT = "$URL_FRIKORT_BASE/harborgerfrikort"
+val URL_FRIKORT_HAR_BORGER_EGENANDELFRITAK = "$URL_FRIKORT_BASE/harborgeregenandelfritak"
+
+val log: Logger = LoggerFactory.getLogger("no.nav.emottak.frikort.rest.FrikortHttpClient")
 
 const val AZURE_AD_AUTH = "AZURE_AD"
 
@@ -108,6 +115,12 @@ fun HttpClientConfig<*>.installFrikortRepoAuthentication() {
 }
 
 suspend fun postHarBorgerFrikort(frikortsporringRequest: FrikortsporringRequest): FrikortsporringResponse {
+    log.asJson(
+        LogLevel.DEBUG,
+        message = "HarBorgerFrikort REST Json request",
+        obj = frikortsporringRequest,
+        serializer = FrikortsporringRequest.serializer()
+    )
     val httpResponse = runCatching {
         frikortHttpClient.post(URL_FRIKORT_HAR_BORGER_FRIKORT) {
             setBody(frikortsporringRequest)
@@ -116,10 +129,23 @@ suspend fun postHarBorgerFrikort(frikortsporringRequest: FrikortsporringRequest)
     }.onFailure { throwable ->
         throw throwable
     }.getOrThrow()
-    return httpResponse.body<FrikortsporringResponse>()
+    return httpResponse.body<FrikortsporringResponse>().also {
+        log.asJson(
+            LogLevel.DEBUG,
+            message = "HarBorgerFrikort REST Json response",
+            obj = it,
+            serializer = FrikortsporringResponse.serializer()
+        )
+    }
 }
 
 suspend fun postHarBorgerEgenandelfritak(frikortsporringRequest: FrikortsporringRequest): FrikortsporringResponse {
+    log.asJson(
+        LogLevel.DEBUG,
+        message = "HarBorgerEgenandelfritak REST Json request",
+        obj = frikortsporringRequest,
+        serializer = FrikortsporringRequest.serializer()
+    )
     val httpResponse = runCatching {
         frikortHttpClient.post(URL_FRIKORT_HAR_BORGER_EGENANDELFRITAK) {
             setBody(frikortsporringRequest)
@@ -128,5 +154,12 @@ suspend fun postHarBorgerEgenandelfritak(frikortsporringRequest: Frikortsporring
     }.onFailure { throwable ->
         throw throwable
     }.getOrThrow()
-    return httpResponse.body<FrikortsporringResponse>()
+    return httpResponse.body<FrikortsporringResponse>().also {
+        log.asJson(
+            LogLevel.DEBUG,
+            message = "HarBorgerEgenandelfritak REST Json response",
+            obj = it,
+            serializer = FrikortsporringResponse.serializer()
+        )
+    }
 }
