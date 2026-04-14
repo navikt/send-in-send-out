6b0ddfb516123f3971e458e88897893faf3f3b2f
Aage Kramarics <Aage.Kramarics@nav.no>
2026-04-14 09:50:22 +0200

    Flyttet feature-flagg til ekstern NAIS config

--- ebms-send-in/src/main/kotlin/no/nav/emottak/App.kt
+++ ebms-send-in/src/main/kotlin/no/nav/emottak/App.kt
@@ -30,6 +30,12 @@ import no.nav.emottak.utils.kafka.client.EventPublisherClient
 import no.nav.emottak.utils.kafka.service.EventLoggingService
 import org.slf4j.LoggerFactory
 
+// De fleste env-variablene hentes fra nais-yaml i dette prosjektet, så key i koden skal stemme med key i nais-yaml.
+// For de følgende feature-flaggene hentes variabelverdien fra ekstern nais-config, med følgende keys.
+// (Se https://console.nav.cloud.nais.io/team/team-emottak/dev-fss/config/ebms-send-in)
+const val USE_ASYNC_IN_KEY = "USE_ASYNC_IN"
+const val USE_ASYNC_OUT_KEY = "USE_ASYNC_OUT"
+
 internal val log = LoggerFactory.getLogger("no.nav.emottak.App")
 
 fun main() = SuspendApp {
@@ -66,14 +72,14 @@ suspend fun ResourceScope.setupServer() {
         config().kafka
     )
 
-    val useAsyncIn = getEnvVar("USE_ASYNC_IN", "false").toBoolean()
+    val useAsyncIn = fixEnvStringFromConfig(getEnvVar(USE_ASYNC_IN_KEY, "false")).toBoolean()
     if (useAsyncIn) {
         log.info("Set up to read asynchronous inbound messages from EbmsInPayload topic")
         eventRegistrationScope.launchEbmsInPayloadReceiver(config(), eventRegistrationService, prometheusMeterRegistry, trekkopplysningService)
     } else {
         log.info("Asynchronous inbound messages turned OFF, will only receive synchronous calls")
     }
-    val useAsyncOut = getEnvVar("USE_ASYNC_OUT", "false").toBoolean()
+    val useAsyncOut = fixEnvStringFromConfig(getEnvVar(USE_ASYNC_OUT_KEY, "false")).toBoolean()
     if (useAsyncOut) {
         log.info("Set up to read asynchronous responses/outbound messages from Fellesformat topic")
         eventRegistrationScope.launchEbmsOutFellesformatReceiver(config(), eventRegistrationService, outPayloadProducer)
@@ -100,4 +106,13 @@ internal fun Application.ebmsSendInModule(
     configureAuthentication()
     configureCoroutineDebugger()
     configureRoutes(prometheusMeterRegistry, eventRegistrationService, trekkopplysningService, useAsyncIn)
+}
+
+// Boolske verdier i ekstern NAIS config må/bør være tekst-strenger, ellers kan de ikke redigeres
+// De kommer da inn til applikasjonen med anførselstegnene i tekstverdien, som må fjernes for å kunne tolkes riktig.
+internal fun fixEnvStringFromConfig(s: String): String {
+    if (s != null && s.startsWith("\"") && s.endsWith("\"")) {
+        return s.replace("\"", "")
+    }
+    return s
 }
