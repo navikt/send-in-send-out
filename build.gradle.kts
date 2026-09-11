30d61ccac5eb55de5e5c21509c8b3fffc1d54c99
Thomas Burnett <7643677+thburnett@users.noreply.github.com>
2026-09-11 10:17:05 +0200

    Bump ktor (#172)
    Co-authored-by: Thomas Burnett <thburnett@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     val kotlinVersion = "2.4.10"
     kotlin("jvm") version kotlinVersion apply false
     kotlin("plugin.serialization") version kotlinVersion // apply false
-    id("io.ktor.plugin") version "3.5.1" apply false
+    id("io.ktor.plugin") version "3.5.2" apply false
     id("org.jlleitschuh.gradle.ktlint") version "11.6.1"
     id("org.jlleitschuh.gradle.ktlint-idea") version "11.6.1"
 }
