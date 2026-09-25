19a0f6f811dfe0abc1b4d55d9f9fffa58441e2b7
Thomas Burnett <7643677+thburnett@users.noreply.github.com>
2026-09-25 16:03:09 +0200

    Legger til service og action i mdc data (#181)
    * Legger til service og action i mdc data
    
    * ktlint
    
    ---------
    
    Co-authored-by: Thomas Burnett <thburnett@users.noreply.github.com>

--- ebms-send-in/src/main/kotlin/no/nav/emottak/ebms/kafka/EbmsInPayloadReceiver.kt
+++ ebms-send-in/src/main/kotlin/no/nav/emottak/ebms/kafka/EbmsInPayloadReceiver.kt
@@ -11,6 +11,7 @@ import kotlinx.coroutines.slf4j.MDCContext
 import kotlinx.coroutines.withContext
 import kotlinx.serialization.json.Json
 import no.nav.emottak.config.Config
+import no.nav.emottak.ebms.route.mdcData
 import no.nav.emottak.ebms.service.FagmeldingService
 import no.nav.emottak.legemelding.LegeMeldingService
 import no.nav.emottak.sykmelding.SyfoMeldingService
@@ -110,12 +111,8 @@ private suspend fun processMessage(
     log.debug("Inner Payload contents: ${sendInRequest.payload.decodeToString()}")
 
     val mdcData = mapOf(
-        "record_key" to recordKey,
-        "messageId" to sendInRequest.messageId,
-        "conversationId" to sendInRequest.conversationId,
-        "cpaId" to sendInRequest.cpaId,
-        "requestId" to sendInRequest.requestId
-    )
+        "record_key" to recordKey
+    ) + sendInRequest.mdcData()
 
     return withContext(MDCContext(mdcData)) {
         FagmeldingService.processRequestAsynchronously(
